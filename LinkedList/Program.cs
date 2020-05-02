d08996fdb66418738629ff6da735c7e0a8cf4fe7
dreamofmaks <dreamofmaks@ukr.net>
2020-05-02 16:42:04 +0300

    Lonked List

---
+++ LinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            var list = new LinkedList<int>();
+
+          
+            list.Add(1);
+            list.Add(5);
+            list.Add(17);
+            list.Add(42);
+            list.Add(-69);
+
+           
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            list.Delete(17);
+
+            
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
